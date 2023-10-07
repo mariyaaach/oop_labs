f7198318945eb415af0fc106511ee8e3c7a2b490
mariyaaaach <116644691+mariyaaach@users.noreply.github.com>
2023-10-07 10:28:22 +0000

    Fixed bugs

--- src/lab0/lib.cpp
+++ src/lab0/lib.cpp
@@ -4,63 +4,67 @@
 
 using namespace std;
 
-// Конструкторы
+// Конструкторы класса Money
+
+// Конструктор по умолчанию
 Money::Money() : length(0), amount(nullptr) {}
 
+// Конструктор с использованием списка инициализации
 Money::Money(initializer_list<unsigned char> initList) : length(initList.size()) {
-    amount = new unsigned char[length];
+    amount = new unsigned char[length]; // Выделение памяти для массива amount
 
     size_t i = 0;
     for (auto it = initList.begin(); it != initList.end(); ++it, ++i) {
-        amount[i] = *it;
+        amount[i] = *it; // Заполнение массива amount значениями из списка инициализации
     }
 }
 
+// Конструктор с использованием строки
 Money::Money(const string& str) : length(str.length()) {
-    amount = new unsigned char[length];
+    amount = new unsigned char[length]; // Выделение памяти для массива amount
 
     size_t i = 0;
     for (auto it = str.rbegin(); it != str.rend(); ++it, ++i) {
-        amount[i] = *it - '0';
+        amount[i] = *it - '0'; // Заполнение массива amount значениями из строки, преобразованными в числа
     }
 }
 
-// Деструктор
+// Деструктор класса Money
 Money::~Money() {
-    delete[] amount;
+    delete[] amount; // Освобождение памяти, выделенной для массива amount
 }
 
-// Оператор сложения
+// Перегрузка оператора сложения
 Money Money::operator+(const Money& other) const {
-    size_t maxLength = max(length, other.length);
-    size_t minLength = min(length, other.length);
+    size_t maxLength = max(length, other.length); // Определение максимальной длины суммируемых сумм
+    size_t minLength = min(length, other.length); // Определение минимальной длины суммируемых сумм
 
-    Money result;
-    result.length = maxLength;
-    result.amount = new unsigned char[maxLength];
+    Money result; // Создание нового объекта Money для хранения результата сложения
+    result.length = maxLength; // Установка длины результата
+    result.amount = new unsigned char[maxLength]; // Выделение памяти для массива amount результата
 
-    unsigned char carry = 0;
+    unsigned char carry = 0; // Переменная для хранения переноса
     for (size_t i = 0; i < minLength; i++) {
-        unsigned int sum = amount[i] + other.amount[i] + carry;
-        result.amount[i] = sum % 10;
-        carry = sum / 10;
+        unsigned int sum = amount[i] + other.amount[i] + carry; // Сложение цифр сумм и переноса
+        result.amount[i] = sum % 10; // Запись последней цифры суммы в результат
+        carry = sum / 10; // Определение нового значения переноса
     }
 
-    const unsigned char* longerAmount = (length > other.length) ? amount : other.amount;
+    const unsigned char* longerAmount = (length > other.length) ? amount : other.amount; // Определение массива с большей длиной
     for (size_t i = minLength; i < maxLength; i++) {
-        unsigned int sum = longerAmount[i] + carry;
-        result.amount[i] = sum % 10;
-        carry = sum / 10;
+        unsigned int sum = longerAmount[i] + carry; // Сложение цифры суммы с переносом
+        result.amount[i] = sum % 10; // Запись последней цифры суммы в результат
+        carry = sum / 10; // Определение нового значения переноса
     }
 
     if (carry > 0) {
-        result.length++;
-        unsigned char* tempAmount = new unsigned char[result.length];
+        result.length++; // Увеличение длины результата
+        unsigned char* tempAmount = new unsigned char[result.length]; // Выделение временной памяти для нового массива amount
 
-        memcpy(tempAmount, result.amount, (result.length - 1) * sizeof(unsigned char));
-        tempAmount[result.length - 1] = carry;
+        memcpy(tempAmount, result.amount, (result.length - 1) * sizeof(unsigned char)); // Копирование данных из старого массива в новый
+        tempAmount[result.length - 1] = carry; // Добавление последней цифры переноса в конец нового массива
 
-        delete[] result.amount;
+        delete[] result.amount; // Освобождение памяти, выделенной для старого массива amount
         result.amount = tempAmount;
     }
 
@@ -69,27 +73,33 @@ Money Money::operator+(const Money& other) const {
 
 // Оператор вычитания
 Money Money::operator-(const Money& other) const {
+    // Проверяем, если текущая сумма меньше суммы, которую нужно вычесть
     if (*this < other) {
-        throw logic_error("Cannot subtract larger amount from smaller amount");
+        throw logic_error("It is impossible to deduct a larger amount from a smaller one");
     }
 
+    // Создаем новый объект Money для хранения результата
     Money result;
     result.length = length;
     result.amount = new unsigned char[length];
 
     unsigned char borrow = 0;
     for (size_t i = 0; i < length; i++) {
+        // Вычисляем разность между текущими цифрами сумм
         int diff = amount[i] - borrow - ((i < other.length) ? other.amount[i] : 0);
         if (diff < 0) {
+            // Если разность отрицательная, заем 10 у следующей цифры
             diff += 10;
             borrow = 1;
         } else {
             borrow = 0;
         }
 
+        // Сохраняем разность в объекте-результате Money
         result.amount[i] = diff;
     }
 
+    // Удаляем ведущие нули из результата
     while (result.length > 1 && result.amount[result.length - 1] == 0) {
         result.length--;
     }
@@ -105,6 +115,7 @@ bool Money::operator>(const Money& other) const {
         return false;
     }
 
+    // Сравниваем цифры, начиная с наиболее значимой цифры
     for (size_t i = length; i > 0; i--) {
         if (amount[i - 1] > other.amount[i - 1]) {
             return true;
@@ -118,6 +129,7 @@ bool Money::operator>(const Money& other) const {
 
 // Оператор меньше
 bool Money::operator<(const Money& other) const {
+    // Возвращаем true, если текущая сумма меньше другой суммы
     return !(*this > other) && !(*this == other);
 }
 
@@ -127,6 +139,7 @@ bool Money::operator==(const Money& other) const {
         return false;
     }
 
+    // Сравниваем каждую цифру сумм
     for (size_t i = 0; i < length; i++) {
         if (amount[i] != other.amount[i]) {
             return false;
@@ -139,6 +152,7 @@ bool Money::operator==(const Money& other) const {
 // Метод для печати денежной суммы
 void Money::print() const {
     for (int i = length - 1; i >= 0; i--) {
+        // Преобразуем тип unsigned char в int для печати
         cout << static_cast<int>(amount[i]);
     }
     cout << endl;
@@ -147,13 +161,15 @@ void Money::print() const {
 // Метод для ввода денежной суммы с клавиатуры
 void Money::input() {
     string str;
-    cout << "Enter monetary amount: ";
+    cout << "Enter amount: ";
     cin >> str;
 
+    // Устанавливаем длину суммы и выделяем память
     length = str.length();
     amount = new unsigned char[length];
 
     size_t i = 0;
+    // Преобразуем каждый символ строки в соответствующую цифру
     for (auto it = str.rbegin(); it != str.rend(); ++it, ++i) {
         amount[i] = *it - '0';
     }
