3fe4a16ccf871232c4b31c7eaaca0b7da4967c34
mariyaaaach <116644691+mariyaaach@users.noreply.github.com>
2023-10-07 11:14:05 +0000

    Add smth

--- main.cpp
+++ main.cpp
@@ -11,21 +11,29 @@ int main() {
 
         Money secondAmount;
         secondAmount.input();
-
-        Money finalAmount1 = firstAmount + secondAmount;
-        Money finalAmount2 = firstAmount - secondAmount;
-
-        cout << "Amount1 + Amount2 = ";
-        finalAmount1.print();
-
-        cout << "Amount1 - Amount2 = ";
-        finalAmount2.print();
-
+        if (firstAmount > secondAmount){
+            Money finalAmount1 = firstAmount + secondAmount;
+            Money finalAmount2 = firstAmount - secondAmount;
+            cout << "Amount1 + Amount2 = ";
+            finalAmount1.print();
+            cout << "Amount1 - Amount2 = ";
+            finalAmount2.print();   
+        }
+        else if (firstAmount < secondAmount){
+            Money finalAmount1 = firstAmount + secondAmount;
+            Money finalAmount2 = secondAmount - firstAmount;
+            cout << "Amount1 + Amount2 = ";
+            finalAmount1.print();
+            cout << "Amount2 - Amount1 = ";
+            finalAmount2.print();
+        }
         if (firstAmount > secondAmount) {
             cout << "The first amount is greater than the second" << endl;
-        } else if (firstAmount < secondAmount) {
+        } 
+        else if (firstAmount < secondAmount) {
             cout << "The first amount is less than the second" << endl;
-        } else {
+        } 
+        else {
             cout << "The amounts are equal" << endl;
         }
     } catch (const exception& e) {
