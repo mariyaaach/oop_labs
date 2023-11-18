dac2ef7737b1508a8be73629e65963784a43d32f
mariyaaaach <116644691+mariyaaach@users.noreply.github.com>
2023-11-18 10:20:24 +0000

    add comments

--- main.cpp
+++ main.cpp
@@ -5,21 +5,22 @@
 
 
 int main() {
-    // std::map with custom allocator
     std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>, 10>> myMap;
+    //cоздание экземпляра std::map с созданным аллокатором
+    //rлючами в контейнере будут числа от 0 до 9, значениями - факториалы ключей
     for (int i = 0; i < 10; ++i) {
-        myMap[i] = factorial(i);
+        myMap[i] = factorial(i); //вычисление факториала числа i и добавление значения в контейнер
     }
     for (const auto& pair : myMap) {
+        //вывод на экран всех значений (ключ и значение разделены пробелом), хранящихся в контейнере
         std::cout << pair.first << " " << pair.second << std::endl;
     }
 
-    // Custom container with custom allocator
     MyContainer<int, 5> myContainer;
+    // создание экземпляра своего контейнера для хранения int с размером 5 и собственным аллокатором
     for (int i = 0; i < 10; ++i) {
-        myContainer.push_back(i);
+        myContainer.push_back(i); //добавление элемента в контейнер
     }
-    myContainer.print();
-
+    myContainer.print(); //вывод на экран всех элементов контейнера
     return 0;
 }
